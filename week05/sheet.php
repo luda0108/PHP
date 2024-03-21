ad7e3e2b5f611472577d230e51c5bd5f6fa56096
luda01088 <125734881+luda0108@users.noreply.github.com>
2024-03-21 09:25:43 +0800

    Create sheet.php

---
+++ week05/sheet.php
@@ -0,0 +1,37 @@
+<html>
+<head>
+<meta charset="utf8">
+</head>
+<title>資管晚會</title>
+<form action="result.php" method="post">
+<center> <font size ="30" color = "black"><b>資管晚會報名表單 </b></font></center>
+活動時間：12月24日(五) 18:00<br/>
+報名時間：即日起至7月底前<br/>
+活動地點：管院B102<br/>
+
+<b><font size="1" color ="red">※以下問題皆須回答，勿留白</b>
+<br/>
+<hr color="black" width="20%" align="left">
+<p>
+<font color ="black">姓名：<input type ="text" name="sName" value="" placeholder="Please input your name"><br/>
+學號：<input type ="text" name="sStudentID" value="" placeholder="Please input your studentID"><br/>
+是否參加資管晚會：
+<input type="radio" name="sWilling" value="true">是
+<input type="radio" name="sWilling" value="false">否<br/>
+你是幾家的：
+<select name="sHouse">
+<option value="一家" selected>first House
+<option value="二家">second house
+<option value="三家">third house
+<option value="四家">fourth house
+<option value="五家">fifth house
+<option value="六家">sixth house
+</select>
+</p>
+<center><font color="red">～表單已到底 感謝大家參與～</font></center>
+<br/>
+<center><input type="submit" value="送出">
+<input type="reset" value="清除"></center>
+</form>
+
+</html>
